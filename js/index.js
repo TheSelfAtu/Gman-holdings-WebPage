6d6f987a618b48a32349a2540c8afd06166b823c
TheSelfAtu <r673774d72k77e@gmail.com>
2020-04-13 11:31:29 +0900

    nav bag fixed 4/13

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
 var startPos = 0,
     winScrollTop = 0;
-var windowWidth = window.innerWidth;
+var windowWidth = window.outerWidth;
 
-// 小さい画面のときはナビバーをfixedにしない,logoの大きさを小さく
+
 $(document).ready(function () {
-    if (windowWidth < 768) {
+    //７６８px以下の時
+    if (windowWidth < 1200) {
+        // ナビバーをfixedにしない,logoの大きさを小さく
         $('.cb-header').removeClass("cb-header")
-        $('nav>.navbar-brand').css("font-size",'1rem')
+        $('nav>.navbar-brand').css("font-size", '1rem')
+        //パララックスをやめ画像に置き換え
+        $('.alt-parallax').css("display", "flex")
+        $('.parallax').css("display", "none")
 
-}
+    }
 });
-  
+
 
 // ナビバー　下にスクロールで出現、上にスクロールで消える
 $(window).on('scroll', function () {
     winScrollTop = $(this).scrollTop();
-    if (windowWidth > 768) {
+    if (windowWidth >= 1200) {
 
         if (winScrollTop < startPos) {
             $('.cb-header').slideUp();
@@ -23,26 +28,18 @@ $(window).on('scroll', function () {
 
             $('.cb-header').slideDown();
         }
-    
+        startPos = winScrollTop;
 
-    if (winScrollTop === 0) {
-        $('.cb-header').slideUp();
+
+        if (winScrollTop === 0) {
+            $('.cb-header').slideUp();
+        }
+
     }
-    startPos = winScrollTop;
-}
 });
-
+// ナビバーでクリック後にドロップダウンメニューを隠す
 $('.navbar-nav>li>a , .dropdown-menu>a').on('click', function () {
     if (this.id != 'navbarDropdown') {
         $('.navbar-collapse').collapse('hide');
     }
-});
-
-// 小さい画面のときはナビバーをfixedにしない,logoの大きさを小さく
-$(document).ready(function () {
-    if (windowWidth < 768) {
-        $('.alt-parallax').css("display","flex")
-        $('.parallax').css("display","none")
-
-}
 });
